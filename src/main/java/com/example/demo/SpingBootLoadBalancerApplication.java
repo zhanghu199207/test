e054fbc8c8ed9c790952177abfeabb6bd580b06c
zhanghu199207 <zhanghu199207@163.com>
2020-03-15 19:35:02 +0800

    1.0版本

--- src/main/java/com/example/demo/SpingBootLoadBalancerApplication.java
+++ src/main/java/com/example/demo/SpingBootLoadBalancerApplication.java
@@ -13,7 +13,9 @@ public class SpingBootLoadBalancerApplication {
 
 	public static void main(String[] args) {
 
-
+/**
+ *   1.0 分支
+ */
 		  Integer add=0;
 		SpringApplication.run(SpingBootLoadBalancerApplication.class, args);
 	}
